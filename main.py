cb24853a15444b072fff733d6481e60225ba96ee
John Gibson <gibbyje2@gmail.com>
2022-02-23 18:46:12 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,202 @@
+import torch
+import torch.utils.data
+import pytorch_lightning as pl
+import torch.nn.functional as F
+from ngp import *
+import utils
+import scipy
+from datasets import *
+import trimesh
+import skimage
+import PIL
+import matplotlib.pyplot as plt
+
+
+def train_obj():
+    num_freqs = 6
+    obj_path = pathlib.Path("data/sdf/dragon.obj")
+    batch_size = 512
+    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    train_dataset = OBJDataset(str(obj_path), num_samples=2**22, tsdf_min=-0.25, tsdf_max=0.25, voxel_resolution=512)
+    val_dataset = OBJDataset(str(obj_path), num_samples=2**16, tsdf_min=-0.25, tsdf_max=0.25, voxel_resolution=512)
+    model = SDFNGPModel(pos_enc_freqs=num_freqs)
+    model.to(dev)
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=8,
+        pin_memory=True
+    )
+
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=8,
+        pin_memory=True
+    )
+
+    trainer = pl.Trainer(
+        gpus=1,
+        logger=True,
+        callbacks=[
+            pl.callbacks.EarlyStopping(monitor="validation/loss", mode="min", patience=3),
+            pl.callbacks.ModelCheckpoint(monitor="validation/loss", mode="min"),
+        ],
+        enable_checkpointing=True,
+        min_epochs=1,
+        max_epochs=20,
+        precision=32 if dev == torch.device("cpu") else 16
+    )
+
+    trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
+    out_path = obj_path.parent / (obj_path.stem + "_pred.ply")
+    predict_mesh(val_dataset, model, str(out_path))
+
+
+def train_sdf():
+    num_freqs = 6
+    sdf_path = pathlib.Path("data/sdf/bunny_nonormals.sdf")
+    batch_size = 256
+    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    train_dataset = SDFDataset(str(sdf_path), tsdf_min=-0.25, tsdf_max=0.25)
+    val_dataset = SDFDataset(str(sdf_path), tsdf_min=-0.25, tsdf_max=0.25)
+    model = SDFNGPModel(pos_enc_freqs=num_freqs)
+    model.to(dev)
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=8,
+        pin_memory=True
+    )
+
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=8,
+        pin_memory=True
+    )
+
+    trainer = pl.Trainer(
+        gpus=1,
+        logger=True,
+        callbacks=[
+            pl.callbacks.EarlyStopping(monitor="validation/loss", mode="min", patience=3),
+            pl.callbacks.ModelCheckpoint(monitor="validation/loss", mode="min"),
+        ],
+        enable_checkpointing=True,
+        min_epochs=1,
+        max_epochs=20,
+        precision=32 if dev == torch.device("cpu") else 16
+    )
+
+    trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
+    out_path = sdf_path.parent / (sdf_path.stem + "_pred.ply")
+    predict_mesh(val_dataset, model, str(out_path))
+
+
+def train_super_res():
+    num_freqs = 6
+    img_path = pathlib.Path("data/image/yosemite.jpg")
+    batch_size = 2**15
+    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    train_dataset = ImageDataset(str(img_path))
+    model = SuperResolutionNGPModel(pos_enc_freqs=num_freqs)
+    # model = SuperResolutionNGPModel.load_from_checkpoint("lightning_logs/version_39/checkpoints/epoch=19-step=39999.ckpt")
+    model.to(dev)
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=8,
+        pin_memory=True
+    )
+
+    trainer = pl.Trainer(
+        gpus=1,
+        logger=True,
+        callbacks=[
+            pl.callbacks.EarlyStopping(monitor="training/loss", mode="min", patience=3),
+            pl.callbacks.ModelCheckpoint(monitor="training/loss", mode="min"),
+        ],
+        enable_checkpointing=True,
+        min_epochs=1,
+        max_epochs=20,
+        precision=32 if dev == torch.device("cpu") else 16
+    )
+
+    trainer.fit(model, train_dataloaders=train_dataloader)
+    out_path = img_path.parent / (img_path.stem + "_pred.jpg")
+    predict_image(train_dataset, model, str(out_path), batch_size=batch_size)
+
+
+def predict_mesh(dataset, model, filename):
+    preds = []
+
+    with torch.inference_mode():
+        model.eval()
+
+        for coord_slice_WD3 in dataset.voxel_coords_hwd3:
+            coord_slice_N3 = coord_slice_WD3.reshape(-1, coord_slice_WD3.shape[2]).to(model.device)
+            pred_slice_N1 = model(coord_slice_N3)
+            preds.append(pred_slice_N1.reshape(coord_slice_WD3.shape[:-1]).cpu())
+
+        sdf_pred_hwd = torch.stack(preds, dim=0).squeeze()
+
+        mask = F.interpolate(
+            einops.rearrange(torch.from_numpy(
+                skimage.morphology.ball(sdf_pred_hwd.shape[0] // 2)),
+                             "h w d -> 1 1 h w d"),
+            sdf_pred_hwd.shape,
+            mode="nearest",
+        ).squeeze().bool().cpu().numpy()
+
+        verts, faces, norms, _ = skimage.measure.marching_cubes(
+            sdf_pred_hwd.numpy(),
+            allow_degenerate=False,
+            mask=mask,
+            level=0)
+        mesh = trimesh.Trimesh(vertices=verts, faces=faces, normals=norms)
+        trimesh.exchange.export.export_mesh(mesh, filename, "ply")
+
+
+def predict_image(dataset, model, filename, batch_size=4096):
+    pred_img = torch.zeros(dataset.rgb_hw3.shape, dtype=torch.float)
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=8,
+        pin_memory=True
+    )
+
+    coords_multiplier = torch.Tensor([dataset.rgb_hw3.shape[0] - 1, dataset.rgb_hw3.shape[1] - 1]).unsqueeze(0)
+
+    with torch.inference_mode():
+        model.eval()
+
+        for batch in dataloader:
+            x, _ = batch
+            c = model(x.to(model.device))
+            i = torch.round(x * coords_multiplier).long().cpu()
+            pred_img[i[:, 0], i[:, 1]] = c.cpu()
+
+    pred_img = torch.clamp(pred_img, min=0.0, max=1.0)
+    PIL.Image.fromarray((pred_img.numpy() * 255.0).astype(np.uint8)).save(filename, quality=100)
+    print(f"PSNR: {(20 * np.log10(1.0)) - (10 * np.log10(F.mse_loss(dataset.rgb_hw3.cpu(), pred_img).item()))}")
+
+
+if __name__ == "__main__":
+    # train_super_res()
+    train_obj()
+
---
+++ ngp.py
@@ -0,0 +1,155 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+import torchvision
+import utils
+from matplotlib import pyplot as plt
+import tqdm
+import numpy as np
+import einops
+
+
+class NeuralGraphicsPrimitiveModel(pl.LightningModule):
+    def __init__(self, mlp, dimension=2, levels=16, max_entries_per_level=2 ** 16, feature_dim=2, min_resolution=16,
+                 max_resolution=16384, **kwargs):
+        super().__init__(**kwargs)
+        self.mlp = mlp
+        self.d = dimension
+        self.L = levels
+        self.T = max_entries_per_level
+        self.F = feature_dim
+        self.N_min = min_resolution
+        self.N_max = max_resolution
+
+        b = np.exp((np.log(self.N_max) - np.log(self.N_min)) / (self.L - 1))
+        N_l = torch.Tensor([np.floor(self.N_min * (b ** l)) for l in range(self.L)]).detach()
+        self.register_buffer("N_l", N_l)
+        self.create_hashmap_parameters()
+
+        self.ema = utils.ExponentialMovingAverage(self.parameters(), decay=0.95)
+
+    def create_hashmap_parameters(self) -> None:
+        feat_init_min = -10 ** -4
+        feat_init_max = 10 ** -4
+        shape = (self.L, self.T, self.F)
+        feats = ((feat_init_max - feat_init_min) * torch.rand(shape, device=self.device)) + feat_init_min
+        self.register_parameter("features", torch.nn.Parameter(feats))
+
+    def get_hypercube_vertices(self, low_coords_bld, high_coords_bld):
+        b, l, d = low_coords_bld.shape
+        coords_bl2d = torch.stack((low_coords_bld, high_coords_bld), dim=2)
+        indices_blDd = einops.repeat(torch.cartesian_prod(*([torch.arange(2, device=self.device)] * d)),
+                                     "v d -> b l v d",
+                                     b=b, l=l)
+        vertices_blDd = torch.gather(coords_bl2d, 2, indices_blDd)
+        return vertices_blDd
+
+    def interpolate(self, scaled_coordinates_bld, feats_blDf, vertices_blDd):
+        """
+        :param scaled_coordinates_bld:
+        :param feats_blDf:
+        :param vertices_blDd:
+        :return:
+        """
+        b, l, d = scaled_coordinates_bld.shape
+        # get hypercube main diagonal length, when the coordinates are scaled all voxels
+        # have side length one
+        side_lengths_bl1d = torch.ones((b, l, 1, d), device=self.device)
+        # n-linear interpolation can be taken as the vertex's value times the volume of the
+        # n-dimensional volume with corners defined by the *opposite* vertex and the point in the interior
+        residuals_blDd = side_lengths_bl1d - torch.abs(vertices_blDd - scaled_coordinates_bld.view(b, l, 1, d))
+        # the volume is obviously the reduction along that dimension via multiplication
+        weights_blD1 = einops.reduce(residuals_blDd,
+                                     "b l D d -> b l D 1",
+                                     "prod")
+        # multiply each vertex value by the weights and sum along the vertices
+        interpolated_feats_blf = einops.reduce(feats_blDf * weights_blD1,
+                                               "b l D f -> b l f",
+                                               "sum")
+        return interpolated_feats_blf
+
+    def forward(self, x):
+        """
+        :param x: bx(d+e) position vector in [0,1] for each dimension with bxe additional data concatenated onto feature
+        :return: output of network
+        """
+        coords, eta = x[:, :self.d], x[:, self.d:]
+        scaled_coords_bld = torch.einsum("bd,l->bld", coords, self.N_l)
+        low_coords_bld = torch.floor(scaled_coords_bld).long()
+        high_coords_bld = torch.ceil(scaled_coords_bld).long()
+        vertices_blDd = self.get_hypercube_vertices(low_coords_bld, high_coords_bld)
+        b, l, D, d = vertices_blDd.shape
+
+        feat_indices_lN = einops.rearrange(utils.spatial_hash(vertices_blDd.view(b * l * D, d), self.T),
+                                           "(b l D) -> l (b D)", b=b, l=l, D=D)
+        l_indices = torch.arange(l, dtype=torch.long, device=self.device)
+        feats_blDf = einops.rearrange(self.features[l_indices[:, None], feat_indices_lN, :],
+                                      "l (b D) f -> b l D f", b=b, l=l, D=D)
+        interpolated_feats_blf = self.interpolate(scaled_coords_bld, feats_blDf, vertices_blDd)
+        final_feats_bF = torch.cat((interpolated_feats_blf.flatten(start_dim=1), eta), dim=1)
+
+        return self.mlp(final_feats_bF)
+
+    def step(self, batch, batch_idx, phase):
+        raise NotImplementedError("Implement this for various methods!")
+
+    def training_step(self, train_batch, batch_idx):
+        return self.step(train_batch, batch_idx, "training")
+
+    def validation_step(self, val_batch, batch_idx):
+        return self.step(val_batch, batch_idx, "validation")
+
+    def configure_optimizers(self):
+        self.ema.to(self.device)
+        return torch.optim.AdamW(self.parameters(), betas=(0.9, 0.99), eps=10e-15, weight_decay=10e-6)
+
+    def on_before_zero_grad(self, *args, **kwargs):
+        self.ema.update(self.parameters())
+
+
+class SDFNGPModel(NeuralGraphicsPrimitiveModel):
+    def __init__(self, pos_enc_freqs=6, coords_min=-1.0, coords_max=1.0):
+        mlp = utils.make_mlp(3 * 2 * pos_enc_freqs, 1, hidden_dim=128, hidden_layers=4)
+        super(SDFNGPModel, self).__init__(mlp, dimension=3, feature_dim=2)
+        self.coords_min = coords_min
+        self.coords_max = coords_max
+        self.pos_enc_freqs = pos_enc_freqs
+
+    def forward(self, x):
+        # coords are between [coords_min, coords_max], scale to be between [0, 1]
+        x -= self.coords_min
+        x /= (self.coords_max - self.coords_min)
+        pos_enc_x = utils.pos_encoding(x, self.pos_enc_freqs, dim=1)
+        return super().forward(pos_enc_x)
+
+    def step(self, batch, batch_idx, phase):
+        x, d = batch
+        outputs = self(x)
+        loss = F.l1_loss(outputs.squeeze(), d.squeeze())
+        self.log(f"{phase}/loss", loss)
+        return loss
+
+
+class SuperResolutionNGPModel(NeuralGraphicsPrimitiveModel):
+    def __init__(self, pos_enc_freqs=6, coords_min=0.0, coords_max=1.0):
+        mlp = utils.make_mlp(2 * 2 * pos_enc_freqs, 3, hidden_dim=64, hidden_layers=2,
+                             output_nonlinearity=torch.nn.Sigmoid())
+        super().__init__(mlp, dimension=2, max_resolution=4096, max_entries_per_level=2**18, feature_dim=2)
+        self.coords_min = coords_min
+        self.coords_max = coords_max
+        self.pos_enc_freqs = pos_enc_freqs
+
+    def forward(self, x):
+        # coords are between [coords_min, coords_max], scale to be between [0, 1]
+        x -= self.coords_min
+        x /= (self.coords_max - self.coords_min)
+        pos_enc_x = utils.pos_encoding(x, self.pos_enc_freqs, dim=1)
+        return super().forward(pos_enc_x)
+
+    def step(self, batch, batch_idx, phase):
+        x, y = batch
+        outputs = self(x)
+        loss = F.mse_loss(outputs.squeeze(), y.squeeze())
+        self.log(f"{phase}/loss", loss)
+        self.log(f"{phase}/psnr", 20 * torch.log10(torch.Tensor([1.0]).to(self.device)) - 10 * torch.log10(loss))
+        return loss
